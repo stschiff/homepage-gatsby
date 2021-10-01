5094b172e8c8404487534dcd8f323892ad9b9533
Stephan Schiffels <stephan_schiffels@mac.com>
2021-10-01 12:40:34 +0200

    further editing

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Row, Col, Badge } from "react-bootstrap"
 import { useStaticQuery, graphql, Link } from "gatsby"
-import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 import dateFormat from "dateformat"
 // import styles from "./sidebar.module.css"
 
 const BlogChunk = ({image_fluid, date, title, link}) => {
-  const imgComp = image_fluid ? <Img fluid={image_fluid} alt="Highlighted image from the blog post" className="border border-dark rounded-lg"/> : <div></div>;
+  const imgComp = image_fluid ? <GatsbyImage image={image_fluid} alt="Highlighted image from the blog post" className="border border-dark rounded-lg"/> : <div></div>;
   return (
     <Row className="border-top py-3 mx-0">
       <Col xs={8} className="pl-0">
@@ -23,7 +23,7 @@ const BlogChunk = ({image_fluid, date, title, link}) => {
 };
 
 const AnnouncementChunk = ({image_fluid, title, date, link}) => {
-  const imgComp = image_fluid ? <Img fluid={image_fluid} alt="Highlighted image for that news item" className="border border-dark rounded-lg"/> : <div></div>;
+  const imgComp = image_fluid ? <GatsbyImage image={image_fluid} alt="Highlighted image for that news item" className="border border-dark rounded-lg"/> : <div></div>;
   return (
     <Row className="border-top py-3 mx-0">
       <Col xs={8} className="pl-0">
@@ -32,7 +32,7 @@ const AnnouncementChunk = ({image_fluid, title, date, link}) => {
           <b><Link to={link}>{title}.</Link></b>
         </p>
       </Col>
-      <Col xs={4} className="pr-0">
+      <Col xs={4} className="rounded">
         {imgComp}
       </Col>
     </Row>
@@ -42,7 +42,7 @@ const AnnouncementChunk = ({image_fluid, title, date, link}) => {
 const PubChunk = ({image_fluid, title, journal, authors, date, citekey}) => {
   let authors_short = authors.length > 2 ? authors[0] + " et al." : (authors.length === 2 ? authors.join(" and ") : authors[0]);
 
-  const imgComp = image_fluid ? <Img fluid={image_fluid} alt="Highlighted image from that publication" className="border border-dark rounded-lg"/> : <div></div>;
+  const imgComp = image_fluid ? <GatsbyImage image={image_fluid} alt="Highlighted image from that publication" className="border border-dark rounded-lg"/> : <div></div>;
 
   const pub_link = "/publications#" + citekey;
   return (
@@ -57,7 +57,7 @@ const PubChunk = ({image_fluid, title, journal, authors, date, citekey}) => {
           </p>
         </p>
       </Col>
-      <Col xs={4} className="pr-0">
+      <Col xs={4} className="rounded">
         {imgComp}
       </Col>
     </Row>
@@ -100,9 +100,7 @@ export default () => {
               isBlogPost
               image {
                 childImageSharp {
-                  fluid(maxHeight: 400, maxWidth: 400) {
-                    ...GatsbyImageSharpFluid
-                  }
+                  gatsbyImageData(layout: FULL_WIDTH, aspectRatio: 1.0)
                 }
               }
             }
@@ -119,9 +117,7 @@ export default () => {
           role
           image {
             childImageSharp {
-              fluid(maxHeight: 400, maxWidth: 400) {
-                ...GatsbyImageSharpFluid
-              }
+              gatsbyImageData(layout: FULL_WIDTH, aspectRatio: 1.0)
             }
           }
         }
@@ -137,7 +133,7 @@ export default () => {
       <h3>Recent News</h3>
       {allSidebarObjects.map(obj => {
         if(obj.type === "Blog") {
-          const img = obj.node.childMdx.frontmatter.image ? obj.node.childMdx.frontmatter.image.childImageSharp.fluid : null;
+          const img = obj.node.childMdx.frontmatter.image ? obj.node.childMdx.frontmatter.image.childImageSharp.gatsbyImageData : null;
           return (
             <BlogChunk link={obj.node.childMdx.fields.slug}
                        title={obj.node.childMdx.frontmatter.title}
@@ -145,7 +141,7 @@ export default () => {
                        date={obj.date}/>
           );
         } else if(obj.type === "Publication") {
-          const img = obj.node.image ? obj.node.image.childImageSharp.fluid : null;
+          const img = obj.node.image ? obj.node.image.childImageSharp.gatsbyImageData : null;
           return (
             <PubChunk image_fluid={img}
                       title={obj.node.title}
@@ -155,7 +151,7 @@ export default () => {
                       citekey={obj.node.citekey}/>  
           );
         } else {
-          const img = obj.node.childMdx.frontmatter.image ? obj.node.childMdx.frontmatter.image.childImageSharp.fluid : null;
+          const img = obj.node.childMdx.frontmatter.image ? obj.node.childMdx.frontmatter.image.childImageSharp.gatsbyImageData : null;
           return (
             <AnnouncementChunk link={obj.node.childMdx.fields.slug}
                                title={obj.node.childMdx.frontmatter.title}
